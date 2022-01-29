beed84a35acf5b6f0794a1493cde33e45c7d264c
jogarao06 <joga123123@gmail.com>
2022-01-29 10:04:25 +0530

    ok

---
+++ src/Java_Programming_Arrys/max_two_num_in_Array.java
@@ -0,0 +1,23 @@
+package Java_Programming_Arrys;
+
+public class max_two_num_in_Array {
+
+	public static void main(String[] args) {
+		int num[] = {5,34,78,2,45,1,99,23};
+		 int maxOne = 0;
+	        int maxTwo = 0;
+			for(int n:num){
+	            if(maxOne < n){
+	                maxTwo = maxOne;
+	                maxOne =n;
+	            } else if(maxTwo < n){
+	                maxTwo = n;
+	            }
+	        }
+	        System.out.println("First Max Number: "+maxOne);
+	        System.out.println("Second Max Number: "+maxTwo);
+	    }
+	     
+	 
+
+}
--- src/java_programming/alternat_uperand_lowecase.java
+++ src/java_programming/alternat_uperand_lowecase.java
@@ -1,11 +1,11 @@
 package java_programming;
 
-
 public class alternat_uperand_lowecase {
 
 	public static void main(String[] args) {
-		
+
 		String str = "jogga";
+		
 		char[] ch = str.toCharArray();
 		for (int i = 0; i < ch.length; i = i + 2) {
 			ch[i] = Character.toUpperCase(ch[i]);
